cca2f34df41bea4d3951b83da650635cea9f4b7b
Mark Briscoe <mark@mcmw.co.uk>
2025-02-18 09:12:09 +0000

    General edits

--- sets.py
+++
@@ -1,7 +0,0 @@
-a = set({1, 2, 3, 4, 5, 2, 4})
-b = set({6, 2, 7, 4, 8, 7, 6})
-
-print(f"Union: {a | b}")
-print(f"Intersection: {a & b}")
-print(f"Difference: {a - b}")
-print(f"Symmetrical Difference: {a ^ b}")
